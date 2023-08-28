b23baa7d88dc9ebe7dd150694c86e86e05b42154
mathanlalsait <mathanlalsait@gmail.com>
2023-08-28 18:33:56 +0530

    base 2

---
+++ JpaRepoFinal/src/main/java/com/candidjava/springboot/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.candidjava.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.candidjava.springboot.entity.User;
+import com.candidjava.springboot.service.UserService;
+
+
+//Swagger url : http://localhost:8082/swagger-ui.html
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	//Create User
+	@RequestMapping(method = RequestMethod.POST, value="/createUser")
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+
+	//Update User
+	@RequestMapping(method = RequestMethod.PUT, value="/updateUser/{id}")
+	public User updateUser(@PathVariable Long id, @RequestBody User user) {
+		return userService.updateUser(id, user);
+	}
+
+	//Get Users
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> getUsers() {
+		return userService.getUsers();
+	}
+
+	//Get User By Id
+	@RequestMapping(method = RequestMethod.GET, value="/getUserById/{id}")
+	public User getUserById(@PathVariable Long id) {
+		return userService.getUserById(id);
+	}
+
+	
+	//Delete User
+	@DeleteMapping("/delete/{id}")
+	@RequestMapping(method = RequestMethod.DELETE, value="/deleteUser/{id}")
+	public void deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
+	}
+
+}
---
+++ JpaRepoFinal/src/main/java/com/candidjava/springboot/entity/User.java
@@ -0,0 +1,67 @@
+package com.candidjava.springboot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	public User() {
+	}
+	@Id
+	@Column
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private String password;
+	@Column
+	private int age;
+	@Column
+	private String email;
+	@Column
+	private String city;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+
+}
