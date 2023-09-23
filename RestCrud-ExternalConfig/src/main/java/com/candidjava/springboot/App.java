613463498efa3d2a5a48bc7c0224a8975a7c2764
Balaji R <balajiradhakrishnan666@gmail.com>
2023-09-23 20:49:09 +0530

    yaml file changed

--- RestCrud-ExternalConfig/src/main/java/com/candidjava/springboot/App.java
+++ RestCrud-ExternalConfig/src/main/java/com/candidjava/springboot/App.java
@@ -1,16 +1,20 @@
 package com.candidjava.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class App {
+	
+    
 	public static void main(String[] args) {
 		
 		System.setProperty("spring.config.location", System.getProperty("user.home") + "/candid/application.yml");
-		System.setProperty("spring.application.name", "${application.name}");
-		System.setProperty("server.port", "${application.portNumber}");
+	//	System.setProperty("server.port", "${application.portNumber}");
+		
+		
 		SpringApplication.run(App.class, args);
 	}
 }
